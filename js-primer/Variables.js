81ac786ad58a37a2018eaca53b9af7b6473eba67
AN-JOLA <anjolaoluwa.olowokere@pau.edu.ng>
2022-04-18 22:35:23 +0100

    Manual 1

--- js-primer/Variables.js
+++ js-primer/Variables.js
@@ -1,7 +1,23 @@
+//Declaring Variables
 var a;
 a = 10;
 b = 11;
 var c = 12;
 
 console.log(a); 
-console.log(b); 
+console.log(b);
+console.log(c);
+
+//Mutablilty of Variables
+a = 13;
+console.log(a);
+a = "Another value which is a string";
+console.log(a);
+a = true;
+console.log(a);
+
+//Primitive Datatypes e.g null & undefined
+var d;
+console.log(d);
+a = null;
+console.log(a);
