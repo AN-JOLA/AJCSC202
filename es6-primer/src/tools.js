0f9d29dd59e457d96960a52678ebde54c54d7ccc
AN-JOLA <joseph.ariyibi@pau.edu.ng>
2022-06-06 10:03:58 +0100

    Progress

--- es6-primer/src/tools.js
+++ es6-primer/src/tools.js
@@ -1,4 +1,4 @@
-var logger = function(output)
+var logger = (output) =>
     {
     console.log(output)
     }
@@ -10,4 +10,44 @@ var logger = function(output)
     }
 
     export default logger;
+
+var logger = output => 
+    {
+    console.log(output)
+    }
+
+var functionWithNoParameter = () => 
+    {
+    console.log("No parameter sent")
+    }
+
+var functionWithTwoOrMoreParameters = (param1, param2) =>
+    {
+    console.log("This has two parameters")
+    }
+
+//Lexical Block Scoping 
+export const dummyFunction = () =>{
+    let feedback = "I am a dummy function";
+    feedback = "I am still a dummy function";
+    return feedback;
+}
+
+try{
+    logger(multiplier(2));
+
+}catch(error){
+    logger(`Error : {Error message}`)
+}
+
+//static methods 
+export class CustomMath{ 
+    static sqrt(a){ 
+    return Math.sqrt(a); 
+   265
+    } 
+    static pow(a,b){ 
+    return Math.pow(a,b); 
+    } 
+   }
 
